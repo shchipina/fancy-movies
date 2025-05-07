be5bc6bf5ede963e2f371e605a686708ad9b15bf
Olena Shchipina <shipina.olena@gmail.com>
2025-05-07 14:31:32 +0300

    add env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSmntKQhEjsO1QNABDxIA7eSzmBX3V7po",
-  authDomain: "netflix-clone-a1087.firebaseapp.com",
-  projectId: "netflix-clone-a1087",
-  storageBucket: "netflix-clone-a1087.firebasestorage.app",
-  messagingSenderId: "620468263380",
-  appId: "1:620468263380:web:30321f73a3393a56441e41"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
